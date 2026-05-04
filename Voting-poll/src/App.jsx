ec01ef72bc3ee801b1da5a1f4f3d1e86586e9313
hiel <hiel.sang@student.moringaschool.com>
2026-05-04 13:05:51 +0300

    Fix GitHub Pages deployment

--- Voting-poll/src/App.jsx
+++ Voting-poll/src/App.jsx
@@ -69,7 +69,7 @@ function App() {
 
   return (
     <main className="mx-auto max-w-md space-y-6 p-4">
-        <h1 main className=" text-center text-3xl text-white font-bold ">Voting Poll App</h1>
+        <h1 className=" text-center text-3xl text-white font-bold ">Voting Poll App</h1>
 
         <PollForm addOption={addOption} />
 
--- Voting-poll/vite.config.js
+++ Voting-poll/vite.config.js
@@ -4,6 +4,7 @@ import babel from '@rolldown/plugin-babel'
 import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/VOTING-POLL/' : '/',
   plugins: [
     react(),
     babel({ presets: [reactCompilerPreset()] }),
